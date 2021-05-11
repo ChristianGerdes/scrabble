98154a24190a2b1dd7e3a4654ebab85bd8edeec6
Tobias Svendsen <tobias.svendsen@gmail.com>
2021-05-11 17:54:26 +0200

    Temporary

--- ScrabbleBot/Bot.fs
+++ ScrabbleBot/Bot.fs
@@ -34,18 +34,49 @@ module internal Bot =
             | Some _ -> true
             | None -> false
 
+        let rec backtrace count ((x, y), direction) acc =
+            match (size state.hand) > count with
+            | false -> acc
+            | true ->
+                match direction with
+                | Direction.right when isCoordInUse (x - 1, y) -> acc
+                | Direction.down when isCoordInUse (x, y - 1) -> acc
+                | Direction.right when
+                    not (isCoordInUse (x - 1, y))
+                    && not (isCoordInUse (x - 2, y)) ->
+                    let newAcc = ((x - 1, y), Direction.right) :: acc
+
+                    // printf "X %A\n" count
+
+                    backtrace (count + 1u) ((x - 1, y), Direction.right) newAcc
+                | Direction.down when
+                    not (isCoordInUse (x, y - 1))
+                    && not (isCoordInUse (x, y - 2)) ->
+                    let newAcc = ((x, y - 1), Direction.down) :: acc
+
+                    // printf "Y %A\n" count
+
+                    backtrace (count + 1u) ((x, y - 1), Direction.down) newAcc
+                | _ -> failwith "HAHA"
+
         let anchorPoints =
-            List.collect
-                (fun ((x, y), _) ->
-                    match ((isCoordInUse (x, y - 1)), (isCoordInUse (x - 1, y))) with
-                    | (true, true) -> []
-                    | (true, false) -> [ ((x, y), Direction.right) ]
-                    | (false, true) -> [ ((x, y), Direction.down) ]
-                    | (false, false) ->
-                        [ ((x, y), Direction.right)
-                          ((x, y), Direction.down) ])
-                (Map.toList state.gameState)
-            |> function
+            // List.fold () acc list
+            let letters =
+                List.collect
+                    (fun ((x, y), _) ->
+                        match ((isCoordInUse (x, y - 1)), (isCoordInUse (x - 1, y))) with
+                        | (true, true) -> []
+                        | (true, false) -> [ ((x, y), Direction.right) ]
+                        | (false, true) -> [ ((x, y), Direction.down) ]
+                        | (false, false) ->
+                            [ ((x, y), Direction.right)
+                              ((x, y), Direction.down) ])
+                    (Map.toList state.gameState)
+
+            let points =
+                List.fold (fun acc anchorPoint -> backtrace 0u anchorPoint acc) letters letters
+
+            match points with
             | [] ->
                 let (x, y) = state.board.center
 
@@ -54,6 +85,14 @@ module internal Bot =
                   (state.board.center, Direction.right)
                   (state.board.center, Direction.down) ]
             | x -> x
+
+        // |> List.collect
+        //     (fun ((x, y), direction) ->
+        //         match direction with
+        //         | Direction.right when not (isCoordInUse (x-1, y)) -> [((x-1, y), Direction.right)]
+        //         | Direction.down when not (isCoordInUse (x, y-1)) -> [((x, y-1), Direction.down)]
+        //         | _ -> failwith "HAHA"
+        //     )
 
         let rec findEdge (x, y) direction =
             match direction with
@@ -134,4 +173,9 @@ module internal Bot =
                     moves
                     hand
 
-        List.fold (fun acc anchorPoint -> findMove state.dict state.hand anchorPoint [] acc) [] anchorPoints
+        List.fold
+            (fun acc anchorPoint ->
+                printf "%A\n" anchorPoint
+                findMove state.dict state.hand anchorPoint [] acc)
+            []
+            anchorPoints
